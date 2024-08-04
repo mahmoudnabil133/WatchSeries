b45448573a392c3248e0796a8f1cccd55765b541
AmiraAHady <amirasgithub@gmail.com>
2024-08-04 20:08:22 +0300

    day 1

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+   userName:string="Amira"
+   userEmail:string="Sincere@april.biz"
+   photo="https://media.istockphoto.com/id/1130884625/vector/user-member-vector-icon-for-ui-user-interface-or-profile-face-avatar-app-in-circle-design.jpg?s=612x612&w=0&k=20&c=1ky-gNHiS2iyLsUPQkxAtPBWH1BZt0PKBB1WBtxQJRE=";
+   isLogin:boolean=false
+
+}
