147c62ca479ddfbd16e0ce9cb1c61bb31d050115
mahmoudnabil133 <hodanabil144@gmail.com>
2024-08-27 17:55:14 +0300

    [Added] user profile

--- src/app/components/my-profile/my-profile.component.ts
+++ src/app/components/my-profile/my-profile.component.ts
@@ -10,6 +10,22 @@ import { RouterLink } from '@angular/router';
   templateUrl: './my-profile.component.html',
   styleUrl: './my-profile.component.css'
 })
-export class MyProfileComponent  {
+export class MyProfileComponent implements OnInit {
+  user: any;
+  constructor(
+    private userService: UserService 
+  ) {};
+
+  ngOnInit(): void {
+    this.userService.myProfile().subscribe({
+      next: (response) => {
+        this.user = response.data;
+        console.log(this.user);
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+  }
 
 }
